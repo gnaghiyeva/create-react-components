43d579764d06f2cf5e32e03f673add1c1c94c96c
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2023-05-01 21:07:12 +0400

    react-create-components

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+const About = () => {
+  return (
+    <>
+    <Navbar/>
+    <div>About Page</div>
+    <Footer/>
+    </>
+  )
+}
+
+export default About
